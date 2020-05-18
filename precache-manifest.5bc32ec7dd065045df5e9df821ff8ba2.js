290c7178eb9c84bdfd76f3ae8a971a44ca45f483
ChernomorYegor <chernomor.yegor@gmail.com>
2020-05-18 22:02:59 +0300

    Updates

---
+++ precache-manifest.5bc32ec7dd065045df5e9df821ff8ba2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c094c1d018e9ecc33e71d44e0b584ad",
+    "url": "/react-web-shop/index.html"
+  },
+  {
+    "revision": "7c063ad5aceece786282",
+    "url": "/react-web-shop/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "7dcf457ab81673321618",
+    "url": "/react-web-shop/static/css/main.881fd8bb.chunk.css"
+  },
+  {
+    "revision": "7c063ad5aceece786282",
+    "url": "/react-web-shop/static/js/2.08a0ba6d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-web-shop/static/js/2.08a0ba6d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7dcf457ab81673321618",
+    "url": "/react-web-shop/static/js/main.e360b1b4.chunk.js"
+  },
+  {
+    "revision": "75d79f8b6a5a1ad47bc2",
+    "url": "/react-web-shop/static/js/runtime-main.b2b1a5aa.js"
+  }
+]);
